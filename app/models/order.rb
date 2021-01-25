6386f373b7b54fdf8e6a670861382ec6a4913f14
Pamela <pamela.tamay@fiuni.edu.py>
2021-01-25 09:01:31 -0300

    Avanzando con la UI de orders index

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :supermarket
   has_many :order_details
   accepts_nested_attributes_for :order_details, :allow_destroy => true
+  delegate :name, :to => :supermarket, :prefix => true
+  enumerize :status, in: [:in_process, :received, :canceled]
 end
